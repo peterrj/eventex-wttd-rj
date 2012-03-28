1ccff7f332f56f1a64e63614b8f31fba6a4a49cd
Peterson Ferreira <doodle.ni@gmail.com>
2012-03-28 10:43:12 -0300

    fix migration

--- src/subscriptions/migrations/0001_initial.py
+++ src/subscriptions/migrations/0001_initial.py
@@ -16,7 +16,6 @@ class Migration(SchemaMigration):
             ('email', self.gf('django.db.models.fields.EmailField')(unique=True, max_length=75)),
             ('phone', self.gf('django.db.models.fields.CharField')(max_length=20, blank=True)),
             ('created_at', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
-            ('paid', self.gf('django.db.models.fields.BooleanField')(default=False)),
         ))
         db.send_create_signal('subscriptions', ['Subscription'])
 
@@ -32,7 +31,6 @@ class Migration(SchemaMigration):
             'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '75'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'paid': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'phone': ('django.db.models.fields.CharField', [], {'max_length': '20', 'blank': 'True'})
         }
     }
